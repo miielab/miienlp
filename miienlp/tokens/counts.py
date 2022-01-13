5544570ecc8edd180d8fea92f1de7c9858dc5572
Callista Nicole Christ <callistac@midway2-login2.rcc.local>
2022-01-13 14:19:26 -0600

    adding first working version of miienlp package

---
+++ miienlp/tokens/counts.py
@@ -0,0 +1,127 @@
+import nltk, spacy
+from nltk.corpus import wordnet
+from miienlp.tokens import utils
+import pandas as pd
+
+class Counts(object):
+    def __init__(self, data, supp_data_path, subcats, method, spacy_dataset="en_core_web_sm", _type="LU"):
+        self.data = data
+        self.spacy_dataset = spacy_dataset
+        self.method = method
+        self.categories = utils.read_vocab_files(supp_data_path, subcats, _type)
+        # TO DO: add method that does specific word counts without using packages
+
+    def generate_spacy_data(self):
+        '''
+        Labels entities using SpaCy and returns dataset
+        '''
+        nlp = spacy.load(self.spacy_dataset)
+        nlp.max_length = 4000000
+        spacy_txt = nlp(self.data)
+        return spacy_txt
+
+    def count_freq_nltk(self):
+        '''
+        Counts the frequency of words that fall into each category using NLTK
+        '''
+        tokens = nltk.word_tokenize(self.data)
+        total_counts = {}
+        for tok in tokens:
+            for key, words in self.categories.items():
+                if tok in words:
+                    if key in total_counts.keys():
+                        total_counts[key] += 1
+                    else:
+                        total_counts[key] = 1
+        # checks for missing keys, sets values to zero
+        for key in self.categories.keys():
+            if key not in total_counts.keys():
+                total_counts[key] = 0
+        return total_counts
+
+    def count_freq_spacy(self, spacy_txt):
+        '''
+        Counts the frequency of words that fall into each category using Spacy
+        '''
+        total_counts = {}
+        for tok in spacy_txt:
+            for key, words in self.categories.items():
+                if tok.text in words:
+                    if key in total_counts.keys():
+                        total_counts[key] += 1
+                    else:
+                        total_counts[key] = 1
+        # checks for missing keys, sets values to zero
+        for key in self.categories.keys():
+            if key not in total_counts.keys():
+                total_counts[key] = 0
+        return total_counts
+
+    def count_prop_nltk(self):
+        '''
+        Counts the number of individual specific words tokens (i.e. "hair":2000) and overall domain counts (i.e. "appearance":3100)
+        '''
+        data = self.data.lower()
+        tokens = nltk.word_tokenize(data)
+        total_counts = {}
+        individual_counts = {}
+
+        for key, words in self.categories.items():
+            individual_counts[key] = {}
+            for word in words:
+                individual_counts[key].update({word:0})
+
+        for key, words in self.categories.items():
+            total_count = 0
+            for word in words:
+                count = tokens.count(word) 
+                total_count += count
+                individual_counts[key][word] = count
+            total_counts[key] = total_count
+        return total_counts, individual_counts
+    
+    def construct_df(self, d):
+        '''
+        Creates dataframe of category and counts
+        '''
+        df = pd.DataFrame(data=d, index=[0])
+        return df.reindex(sorted(df.columns), axis=1)
+
+    def construct_prop_df(self, data, key):
+        '''
+        Creates dataframe of proportions and sorts by the most commonly used words
+        '''
+        df = pd.DataFrame(data, columns=[key, 'count', 'percentage'])
+        return df.sort_values(by=['percentage'], ascending=False)
+
+    def normalize_counts(self, sub_dict):
+        '''
+        Normalizes a dictionary of counts for all specific words in a given category 
+        (i.e. "female" category has counts normalized for "she", "her", "mother", etc)
+        '''
+        data = []
+        total = sum(sub_dict.values(), 0)
+        for word, count in sub_dict.items():
+            perc = 0 if total == 0 else (count / total) * 100
+            data.append([word, count, round(perc, 2)])
+        data.append(['TOTAL', total, 0.]) if total == 0 else data.append(['TOTAL', total, 100.00])
+        return data
+
+    def run_pipeline(self, output_folder="", proportion=False):
+        '''
+        Runs specific word pipleine
+        Proportion: a parameter that optionally outputs whether to include CSV files on which words "drive" (make up) a given category
+        https://docs.google.com/spreadsheets/d/1pvU8KwOrxH22BSoGQywYcYoZXN5-R4SW2sq0G3ZYgDE/edit?usp=sharing
+        '''
+        if self.method == 'spacy':
+            spacy_txt = self.generate_spacy_data()
+            counts = self.count_freq_spacy(spacy_txt)
+        else:
+            counts = self.count_freq_nltk()
+        if proportion:
+            counts, individual_counts = self.count_prop_nltk()
+            for key, sub_dict in individual_counts.items():
+                data = self.normalize_counts(sub_dict)
+                df = self.construct_prop_df(data, key)
+                utils.save_results(df, output_folder + "_proportion_" + key + ".csv")
+        return self.construct_df(counts)
---
+++ miienlp/tokens/main.py
@@ -0,0 +1,78 @@
+#from ner import NER
+from specific_words import SpecificWordCounts
+import utils
+import glob, os
+import pandas as pd
+#from alive_progress import alive_bar
+import argparse
+
+def main(args):
+    params = utils.get_params(args['input'])
+    num_files = len(glob.glob(params['data_dir'] + '/**/*.txt', recursive=True))
+    NER_total_df = pd.DataFrame(columns=["filepath", "entity", "modal_tag", "person_tag", "freq"])
+    specific_words_total_df = pd.DataFrame()
+
+    with alive_bar(num_files, bar="blocks") as bar:
+        for root, subdirs, files in os.walk(params['data_dir']):
+            for _file in files:
+                file_path = os.path.join(root, _file)
+                if not file_path.endswith('.txt'): 
+                    continue
+                data = utils.load_data(file_path)
+                filename = _file.split('/')[-1][:-4]
+                if data is None:
+                    continue
+
+                # running NER analysis
+                if params['ner']['output_dir']:
+                    output_folder = utils.construct_output_filenames(root, params['data_dir'], params['ner']['output_dir'], 'ner', filename)
+                    filter_keywords = "_".join(params['ner']['filter_entities'])
+                    output_filename = output_folder + "_" + filter_keywords + "_NER_counts.csv"
+                    if not os.path.isfile(output_filename):
+                        df = run_ner(data, params, _file, file_path)
+                        utils.save_results(df, output_filename)
+                        #NER_total_df = NER_total_df.append(df) #uncomment this when we want a combined NER dataset (emileigh)
+
+                # running specific words analysis
+                if params['specific_words']['output_dir']:
+                    output_folder = utils.construct_output_filenames(root, params['data_dir'], params['specific_words']['output_dir'], 'specific_words', filename)
+                    output_filename = output_folder + "_word_counts.csv"
+                    if not os.path.isfile(output_filename):
+                        df_counts = run_sw(data, params, _file, file_path, output_folder)
+                        utils.save_results(df_counts, output_filename)
+                bar()
+            # add below codeblock back in when we want to have a large combined NER dataset (emileigh)
+            '''
+            if params['ner']['output_dir']:
+                NER_total_df.reset_index(drop=True, inplace=True)
+                total_output = params['ner']['output_dir'] + '/ner/'
+                utils.construct_output(total_output)
+                utils.save_results(NER_total_df, total_output + 'NER_combined.csv')
+            '''
+
+def run_ner(data, params, _file, file_path):
+    ner = NER(data, params['ner']['filter_entities'], params['ner']['spacy_dataset'])
+    df = ner.run_pipeline()
+    df.insert(0, "filepath", file_path) 
+    df.insert(1, "corpus", file_path.split('/')[-3])
+    df.insert(2, "book_id", _file[:-4])
+    df.insert(3, "spacy_dataset", params['ner']['spacy_dataset']) 
+    return df
+
+def run_sw(data, params, _file, file_path, output_folder=""):
+    swc = SpecificWordCounts(data, params['specific_words']['categorize_file'], params['specific_words']['subcats'], params['specific_words']['method'], spacy_dataset=params['specific_words']['spacy_dataset'], _type=params['specific_words']['type'])
+    df = swc.run_pipeline(output_folder)
+    df.insert(0, "filepath", file_path)
+    df.insert(1, "corpus", file_path.split('/')[-3])
+    df.insert(2, "book_id", _file[:-4]) 
+    df.insert(3, "spacy_dataset", params['ner']['spacy_dataset']) 
+    df.insert(4, "total_words", len(data.split()))
+    return df
+
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-i', "--input", required=False, help="Path to input yaml file")
+    args = vars(ap.parse_args())
+    main(args)
