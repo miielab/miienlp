ca6dc5b72844947aa92a0d65e8c71704708baa99
patriChiril <61833458+patriChiril@users.noreply.github.com>
2022-10-31 16:14:08 -0500

    Update test_wordEmbeddings.py

--- unittest/wordEmbeddings/test_wordEmbeddings.py
+++ unittest/wordEmbeddings/test_wordEmbeddings.py
@@ -3,38 +3,37 @@ sys.path.insert(0, '../../miienlp/embeddings')
 from word_vecs import WordVectors
 #from data_prep import WVecDataPreparation
 
-time_series =\
-{"type": "decade",
- "time_frame": "all"
-}
-
 model =\
 {"name": "word2vec",
   "size": 300,
+  "num_models": 1
   "window": 5,
-  "min_count": 20,
+  "min_count": 10,
   "workers": 5,
   "sg": 1,
   "hs": 1,
   "negative": 0,
   "epochs":5
   }
+
 output =\
-{"output_model_dir": "test_results",
+{"output_model_dir": "../../examples/test_results/",
  "save_vocab_np": True,
  "save_vocab_txt": True
 }
 
-collection_corpora =\
-{"mainstream":["caldecott", "newbery"]
-}
-
-data_dir= "test_data/"
+data_dir= ["../../examples/example_book_excerpt.txt"]
 
 class TestWordEmbedding(object):
-    def test_combine_text(self):
+  '''  
+  def test_combine_text(self):
         dp = WVecDataPreparation(data_dir, time_series, collection_corpora)
         time_frame = dp.construct_time_series()
         df = dp.construct_relevant_file_path_df(time_frame)
         comb_path = dp.combine_text(df)
         assert df.head() == ["time_series"]
+  '''
+  def test_output(self):
+        # tests output filename creation
+        w2v_file = make_w2v_model()
+        assert output_file == "../../examples/test_results/model0.bin"
