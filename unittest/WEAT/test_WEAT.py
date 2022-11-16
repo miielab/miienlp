9985e15601415b018d66ce477c77eeae967461df
patriChiril <61833458+patriChiril@users.noreply.github.com>
2022-11-16 16:32:32 -0600

    Update test_WEAT.py

--- unittest/WEAT/test_WEAT.py
+++ unittest/WEAT/test_WEAT.py
@@ -3,7 +3,7 @@ import os.path
 sys.path.insert(0, '../../miienlp/weat')
 from wordtest import Single_WordTest, T1, T2
 
-parameters =\
+params =\
 {'run_analysis': "t",
 'run_cleaning': "",
 'model_directory': "../../examples/test_data/",
@@ -20,8 +20,19 @@ parameters =\
 class TestWEAT(object):
 
   def test_output(self):
+      fv = FetchVec(params["model_directory"],
+                          params["output_directory"],
+                          params["test_directory"],
+                          params["vocabulary_suffix"],
+                          params["embeddings_suffix"])
+            fv.fetch_vectors()
+
+        wt = Multiple_WordTests(params["test_directory"],
+                                params["output_directory"],
+                                params["output_file"])
+    
         # tests output filename creation
-        weat_scores = T1()
+        weat_scores = T1() #curr_test -- 3 embed, curr_model
         weat_scores.single_full_test()
         
         assert os.path.exists('../../examples/test_data/weat_output_file.json')
